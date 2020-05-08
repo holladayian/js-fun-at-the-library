3896b843ec4d751b0a73f402cab762315df2b6fc
Ian Holladay <holladayian@gmail.com>
2020-05-08 10:45:00 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,10 +1,25 @@
-function createTitle() {
-  
+function createTitle(titleMod) {
+return "The " + titleMod;
+
 }
+createTitle("Storm's Awakening");
+createTitle("Dancing Sushi");
+createTitle("Dragon in the Summer");
+createTitle("Teenage Ghoul");
+
+function buildMainCharacter(name, age, pronouns) {
+  this.name = name;
+  this.age = age;
+  this.pronouns = pronouns;
+
+}
+var ghoulCharacter = new buildMainCharacter("Vassya", 16, "she/her");
+
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
+  buildMainCharacter: buildMainCharacter,
   // saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
